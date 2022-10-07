3487b99334c90066d0467268d63937a7bcafdbe9
Michkasov Roman <mihjob@list.ru>
2022-10-08 01:33:46 +0300

    Made the second changes to the original text of the code

--- Michkasov_function.py
+++ Michkasov_function.py
@@ -7,14 +7,14 @@ def Michkasov_function (number, base):
 
 Michkasov_function (375, 5)
 
-array: int =[1,2,3]
-index: int = 0
-size: int = 3
-element: int = array[0]
+array =[1,2,3]
+index = 0
+size = 3
+element = array[0]
 while index < size:
-    m: int= Michkasov_function (array[index], 2)
+    m = Michkasov_function (array[index], 2)
     element = array[index]
     array[index] = array [m]
-    array [m, 2] = element
+    array [m] = element
     index = index + 1
 print (array)
