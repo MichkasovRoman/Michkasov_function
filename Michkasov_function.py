ac04be59c01ba4b451abef765e30a2c0d97bdc79
MichkasovRoman <115039202+MichkasovRoman@users.noreply.github.com>
2022-10-08 00:59:17 +0300

    Wrote a code to test the action of a function on array elements

--- Michkasov_function.py
+++ Michkasov_function.py
@@ -6,3 +6,13 @@ def Michkasov_function (number, base):
     print (result)
 
 Michkasov_function (375, 5)
+
+array =[1,2,3]
+index = 0
+size = 3
+element = array[0]
+while index < size:
+    index = Michkasov_function (array[index], 2)
+    element = array[index]
+    index = index +1
+print (array)
