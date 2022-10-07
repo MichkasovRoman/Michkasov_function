e9002799a650506bf2f1c41bc851ca3ebba6bce6
Michkasov Roman <mihjob@list.ru>
2022-10-08 01:52:02 +0300

    Made failled changes to the original text of the code

--- Michkasov_function.py
+++ Michkasov_function.py
@@ -5,15 +5,13 @@ def Michkasov_function (number, base):
         result = result + 1
     print (result)
 
-Michkasov_function (375, 5)
-
 array =[1,2,3]
 index = 0
 size = 3
 element = array[0]
 while index < size:
-    m = Michkasov_function (array[index], 2)
-    element = array[index]
-    array[index] = m
+    element = array[index] 
+    array[index] = Michkasov_function (array[index], 2)
+    Michkasov_function (array[index], 2) = element
     index = index + 1
 print (array)
